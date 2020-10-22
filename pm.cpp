2162f09babbd6c9b5110b750f1b899a961840919
victorwu <victor__wu@live.com>
2020-10-22 12:09:11 -0700

    init pub-sub network

---
+++ pm.cpp
@@ -0,0 +1,46 @@
+//
+//  Reading from multiple sockets in C++
+//  This version uses zmq_poll()
+//
+
+#include "zhelpers.hpp"
+
+
+int main ()
+{
+    zmq::context_t context(1);
+
+    zmq::socket_t receiver(context, ZMQ_PULL);
+    receiver.bind("tcp://*:1234");
+
+    zmq::socket_t publisher(context, ZMQ_PUB);
+    publisher.bind("tcp://*:1235");
+
+
+    //  Initialize poll set
+    zmq::pollitem_t items [] = {{ 
+            static_cast<void*>(receiver), 0, ZMQ_POLLIN, 0 
+        }
+    };
+    //  Process messages from both sockets
+    while (1) {
+        
+        zmq::poll (&items [0], 1, 0);
+        
+        if (items[0].revents & ZMQ_POLLIN) {
+            zmq::message_t message (5);
+            receiver.recv(&message);
+            std::cout << "Received from rx " << std::endl;
+
+            std::cout << "Send to tx" << std::endl;
+            zmq::message_t request (12);
+            memcpy (request.data (), "Pub-1 Data", 12);
+            publisher.send (request);
+        }
+        
+        
+        
+        sleep(1);
+    }
+    return 0;
+}
---
+++ tx.cpp
@@ -0,0 +1,47 @@
+//
+//  Reading from multiple sockets in C++
+//  This version uses zmq_poll()
+//
+
+#include "zhelpers.hpp"
+
+
+int main ()
+{
+
+    zmq::context_t context(1);
+    zmq::socket_t subscriber (context, ZMQ_SUB);
+    subscriber.connect("tcp://localhost:1235");
+    subscriber.setsockopt(ZMQ_SUBSCRIBE, "", 0);
+
+    zmq::socket_t push(context, ZMQ_PUSH);
+    push.connect("tcp://localhost:1236");
+
+    zmq::pollitem_t items [] = {{ 
+        static_cast<void*>(subscriber), 0, ZMQ_POLLIN, 0 
+    }};
+
+    while(1) {
+        
+
+        zmq::poll (&items [0], 1, 0);
+
+        if (items[0].revents & ZMQ_POLLIN) {
+            char arr[20] = {0};
+            int rnd = rand() % 100;
+            
+            zmq::message_t update;
+            std::cout << "Received from pm"<< std::endl;
+            subscriber.recv (&update);
+
+            // zmq::message_t message(100);
+            // push.send(message);
+            std::string data = "data" + std::to_string(rnd);
+            zmq_send(push, data.c_str(), data.length(), 0);
+
+            std::cout << "Sent to rx"<< std::endl;
+        }
+
+    }
+    return 0;
+}
